de5452a64db5a766afce1ce8cf3771d407cb93e9
zwh <eueni@163.com>
2020-09-10 18:11:24 +0800

    用then处理generator

---
+++ JavaScript/co/co.js
@@ -0,0 +1,23 @@
+const fs = require("fs").promises;
+
+function* read() {
+  const path = yield fs.readFile("./path.txt", "utf8");
+  console.log("path", path);
+  const name = yield fs.readFile(path, "utf8");
+  console.log("name", name);
+  return name;
+}
+
+const it = read();
+const { value, done } = it.next();
+value.then((data) => {
+  console.log("data1", data);
+
+  const { value, done } = it.next(data);
+  value.then((data) => {
+    console.log("data2", data);
+
+    const { value, done } = it.next(data);
+    console.log("data3", value, done);
+  });
+});
