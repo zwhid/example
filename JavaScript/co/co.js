a8408afd729245ebc35aa1d1abfb3e8a5602ea59
zwh <eueni@163.com>
2020-09-10 18:26:44 +0800

    generator的try-catch

--- JavaScript/co/co.js
+++ JavaScript/co/co.js
@@ -1,42 +1,27 @@
 const fs = require("fs").promises;
 
 function* read() {
-  const path = yield fs.readFile("./path.txt", "utf8");
-  console.log("path", path);
-  const name = yield fs.readFile(path, "utf8");
-  console.log("name", name);
-  return name;
+  try {
+    const path = yield fs.readFile("./path1.txt", "utf8");
+    const name = yield fs.readFile(path, "utf8");
+    return name;
+  } catch (error) {
+    console.log("===\n", error);
+  }
 }
 
-function co(iterator) {
-  const ctx = this;
-  return new Promise((resolve, reject) => {
-    // 如果传入的还是 generator 函数，先执行得到迭代器
-    if (typeof iterator === 'function') iterator = iterator.call(ctx);
+const it = read();
+const { value, done } = it.next();
+value.then((data) => {
+  console.log("data1", data);
 
-    function next(data) {
-      const { value, done } = iterator.next(data);
+  const { value, done } = it.next(data);
+  value.then((data) => {
+    console.log("data2", data);
 
-      if (done) {
-        return resolve(value);
-      }
-
-      // 可能yield返回的是基本值而不是promise，所以用Promise.resolve包装
-      Promise.resolve(value).then(data => {
-          next(data);
-        },err => {
-          reject(err);
-        }
-      );
-
-      // Promise.resolve(value).then(next,reject) // 可以简写为
-    }
-    next();
+    const { value, done } = it.next(data);
+    console.log("data3", value, done);
   });
-}
-
-co(read()).then(data => {
-  console.log("data", data);
 }).catch((err) => {
-  console.log("err", err);
+  it.throw(err); //迭代器手动抛出错误，可以被try catch捕获到
 });
